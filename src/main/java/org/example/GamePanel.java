d6a69c6e0b07ba1f70c3290b139cc55d62524a2e
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2023-07-09 01:41:06 +0300

    Added game panel variables and game panel constructor

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
@@ -10,12 +14,61 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_HEIGHT * SCREEN_WIDTH)/UNIT_SIZE;
+    static final int DELAY = 100;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
     GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+    public void startGame(){
+    }
+
+    public void paintComponent(Graphics g){
+    }
+
+    public void draw(Graphics g){
 
     }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics graphics){
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public class  MyKeyAdapter extends KeyAdapter{
+
+        public void KeyPressed(KeyEvent e){
+
+        }
     }
 }
