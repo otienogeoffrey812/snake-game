0034d5d6540712549efb600e0cdd21c46d312b46
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2023-11-19 13:08:25 +0300

    Added applesEaten and bodyParts counter

--- src/main/java/com/artman/GamePanel.java
+++ src/main/java/com/artman/GamePanel.java
@@ -102,6 +102,11 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkApple(){
+        if((x[0]==appleX)&&(y[0]==appleY)){
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
 
     public void checkCollisions(){
